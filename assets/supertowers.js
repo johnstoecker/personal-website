381fe4a6a0e8a88c5869f4927175c937fae4628f
John Stoecker <jstoecker@cerego.com>
2016-04-05 02:44:03 -0700

    adding supertowers content

--- assets/supertowers.js
+++ assets/supertowers.js
@@ -5,6 +5,7 @@ closeGameContainers = function() {
   $(".game-content-container").each(function() {
     $(this).css("display", "none");
   })
+  $(".game-top-link").children().removeClass("active")
 }
 
 
@@ -12,22 +13,23 @@ $("#game_overview_link").click(function() {
   console.log("game overview link clicked");
   closeGameContainers();
   $("#game_overview").css("display", "inherit")
+  $("#game_overview_link").addClass("active")
 })
-
-
 
 $("#game_how_to_play_link").click(function(){
   closeGameContainers();
   $("#game_how_to_play").css("display", "inherit");
+  $("#game_how_to_play_link").addClass("active")
 })
 $("#game_expansion_link").click(function(){
   closeGameContainers();
   $("#game_expansion").css("display", "inherit");
+  $("#game_expansion_link").addClass("active")
 })
-
-
-
-
-
+$("#game_praise_link").click(function(){
+  closeGameContainers();
+  $("#game_praise").css("display", "inherit");
+  $("#game_praise_link").addClass("active")
+})
 
 })
