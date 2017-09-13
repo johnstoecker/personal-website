65a5cdf533e50121f2c4854dc6081b23885627b4
John Stoecker <johnstoecker@gmail.com>
2017-09-12 21:59:34 -0700

    hermano challenge and eth pullups

---
+++ ethpullups/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+  var prefix, status;
+  var rand = Math.round(Math.random()*5)
+  total = 0
+  $.get( "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD", function( data ) {
+    eths = data.USD * 0.87072
+    ethStatus = "$" + eths.toFixed(2) + " ETH"
+    document.getElementById('kit-ltc-status').innerHTML = ethStatus;
+  })
+  Tabletop.init({
+    key: "1xp9l4himQZeEx9z9dD37IQWp222-QTmpN8Jtx_yUWI4"
+  , callback: function(data,tableTop){
+      document.getElementById('challenge-summary').innerHTML = ""
+      console.log(data);
+      for(var i=0; i < data.length; i++) {
+          console.log(name)
+          var name = data[i].Name
+          if (name && name != "") {
+              var current = parseInt(data[i].Current)
+              var start = parseInt(data[i].Start)
+              var end = parseInt(data[i].End)
+              var numHTML = ""
+              var borderClass = ""
+              if (current < start) {
+                  numHTML = "-" + Math.abs(current - start)
+                  borderClass = "red"
+              } else if (current == start) {
+                  numHTML = "+0"
+                  borderClass = "red"
+              } else if (current >= end) {
+                  numHTML = "+" + current
+                  borderClass = "green"
+              } else {
+                  numHTML = "+" + current
+                  borderClass = "white"
+              }
+
+              var htmlToAdd = '<div class="player-summary ' + borderClass+ '"><div class="player-name">'+name+'</div><div class="player-pullups">'+numHTML+'</div></div>'
+
+              document.getElementById('challenge-summary').innerHTML += htmlToAdd
+          }
+      }
+
+  }
+  , simpleSheet: true})
+
+})
--- hermanochallenge/main.js
+++ hermanochallenge/main.js
@@ -10,4 +10,34 @@ $(document).ready(function() {
     ethStatus = "$" + eths.toFixed(2) + " ETH"
     document.getElementById('kit-ltc-status').innerHTML = ethStatus;
   })
+  Tabletop.init({
+    key: "1Zi7bHxKFCMb_1gR3IvZtcfpLEwv8g1ijtbeZQKM7x_k"
+  , callback: function(data,tableTop){
+      console.log(data);
+      for(var i=0; i < data.length; i++) {
+          var name = data[i].Name
+          var current = parseInt(data[i].Target) - parseInt(data[i].Current) - 4
+          var borderClass = ""
+          if (current < 0) {
+              current = "-" + current
+              borderClass = "red"
+          } else if (current == 0) {
+              current = "+" + current
+              borderClass = "red"
+          } else if (current > 3) {
+              current = "+" + current
+              borderClass = "green"
+          } else {
+              current = "+" + current
+              borderClass = "white"
+          }
+
+          var htmlToAdd = '<div class="player-summary ' + borderClass+ '"><div class="player-name">'+name+'</div><div class="player-pullups">'+current+'</div></div>'
+
+          document.getElementById('challenge-summary').innerHTML += htmlToAdd
+      }
+
+  }
+  , simpleSheet: true})
+
 })
