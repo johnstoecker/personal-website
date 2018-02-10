155b1523af4fb6817264659131e374cf3c179359
John Stoecker <johnstoecker@gmail.com>
2018-02-10 00:57:31 -0800

    give img bot a lil margin

--- img/index.php
+++ img/index.php
@@ -13,18 +13,22 @@
         $width = abs($bbox[4] - $bbox[0]);
         $height = abs($bbox[5] - $bbox[1]);
 
-        $im = imagecreatetruecolor($width, $height+$font_size);
+        $im = imagecreatetruecolor($width + 30, $height+$font_size);
         // Create some colors
         $white = imagecolorallocate($im, 255, 255, 255);
         $black = imagecolorallocate($im, 0, 0, 0);
-        imagefilledrectangle($im, 0, 0, $width - 1, $height + $font_size - 1, $white);
+        imagefilledrectangle($im, 0, 0, $width + 29, $height + $font_size - 1, $white);
 
         // Add the text
-        imagettftext($im, $font_size, 0, 0, $height + $font_size/2, $black, $font, $text);
+        imagettftext($im, $font_size, 0, 15, $height + $font_size/2, $black, $font, $text);
 
         // Using imagepng() results in clearer text compared with imagejpeg()
         imagepng($im);
         imagedestroy($im);
         imagecolordeallocate($white);
         imagecolordeallocate($black);
+        // Get Image content a variable
+        // Clean the output buffer
+
+        //Base64 Encode
 ?>
