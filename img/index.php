41ef4922f997d801f741439a03e5e567ed07ff2a
John Stoecker <johnstoecker@gmail.com>
2020-01-18 23:40:03 -0800

    Update index.php
    Add 8bit and size options for img bot

--- img/index.php
+++ img/index.php
@@ -3,8 +3,14 @@
         // Set the content-type
          header('Content-Type: image/png');
         // Create the image
-        $font_size = 80;
-        $font = './Raleway-Medium.ttf';
+        if ($_GET['size'])
+		$font_size = $_GET['size'];
+	else
+		$font_size = 80;
+        if ($_GET['8bit'])
+		$font = './PxPlus_IBM_VGA9.ttf';
+	else
+		$font = './Raleway-Medium.ttf';
         $text = $_GET['text'];
         if (strlen($text)>500) {
           $text="it's too long my friend";
@@ -13,16 +19,32 @@
         $width = abs($bbox[4] - $bbox[0]);
         $height = abs($bbox[5] - $bbox[1]);
 
-        $im = imagecreatetruecolor($width + 30, $height+$font_size);
-        // Create some colors
-        $white = imagecolorallocate($im, 255, 255, 255);
-        $black = imagecolorallocate($im, 0, 0, 0);
-        imagefilledrectangle($im, 0, 0, $width + 29, $height + $font_size - 1, $white);
-
         // Add the text
-        imagettftext($im, $font_size, 0, 15, $height + $font_size/2, $black, $font, $text);
+        if($_GET['8bit']){
+                $resized_width = 2;
+		while($resized_width < $width + 29){
+			$resized_width = $resized_width * 2;
+		}
+		//TODO: abstract this out into a fn
+		$resized_height = 2;
+		while ($resized_height < $height + $font_size - 1) {
+			$resized_height = $resized_height *2;
+		}
+		$im = imagecreatetruecolor($resized_width, $resized_height);
+        	$white = imagecolorallocate($im, 255, 255, 255);
+        	$black = imagecolorallocate($im, 0, 0, 0);
+		imagefilledrectangle($im, 0, 0, $resized_width, $resized_height, $white);
+		//turn off aliasing for 8bit text
+		imagettftext($im, $font_size, 0, 15, $resized_height-32, -$black, $font, $text);
 
-        // Using imagepng() results in clearer text compared with imagejpeg()
+	} else {
+		$im = imagecreatetruecolor($width + 30, $height+$font_size);
+        	$white = imagecolorallocate($im, 255, 255, 255);
+        	$black = imagecolorallocate($im, 0, 0, 0);
+		imagefilledrectangle($im, 0, 0, $width + 29, $height + $font_size - 1, $white);
+        	imagettftext($im, $font_size, 0, 15, $height + $font_size/2, $black, $font, $text);
+        }
+	// Using imagepng() results in clearer text compared with imagejpeg()
         imagepng($im);
         imagedestroy($im);
         imagecolordeallocate($white);
